7331f93eb1273286c038c51a0662d5ba229c3f9e
ersatzSquare <40550457+ersatzSquare@users.noreply.github.com>
2018-09-08 22:05:59 -0300

    Add files via upload

--- roster.js
+++ roster.js
@@ -1,3 +1,78 @@
+var colorThief = new ColorThief();
+
+function getComplimentary(rgb){
+
+    // Convert hex to rgb
+    // Credit to Denis http://stackoverflow.com/a/36253499/4939630
+   
+    // Get array of RGB values
+
+    var r = rgb[0], g = rgb[1], b = rgb[2];
+
+    // Convert RGB to HSL
+    // Adapted from answer by 0x000f http://stackoverflow.com/a/34946092/4939630
+    r /= 255.0;
+    g /= 255.0;
+    b /= 255.0;
+    var max = Math.max(r, g, b);
+    var min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2.0;
+
+    if(max == min) {
+        h = s = 0;  //achromatic
+    } else {
+        var d = max - min;
+        s = (l > 0.5 ? d / (2.0 - max - min) : d / (max + min));
+
+        if(max == r && g >= b) {
+            h = 1.0472 * (g - b) / d ;
+        } else if(max == r && g < b) {
+            h = 1.0472 * (g - b) / d + 6.2832;
+        } else if(max == g) {
+            h = 1.0472 * (b - r) / d + 2.0944;
+        } else if(max == b) {
+            h = 1.0472 * (r - g) / d + 4.1888;
+        }
+    }
+
+    h = h / 6.2832 * 360.0 + 0;
+
+    // Shift hue to opposite side of wheel and convert to [0-1] value
+    h+= 180;
+    if (h > 360) { h -= 360; }
+    h /= 360;
+
+    // Convert h s and l values into r g and b values
+    // Adapted from answer by Mohsen http://stackoverflow.com/a/9493060/4939630
+    if(s === 0){
+        r = g = b = l; // achromatic
+    } else {
+        var hue2rgb = function hue2rgb(p, q, t){
+            if(t < 0) t += 1;
+            if(t > 1) t -= 1;
+            if(t < 1/6) return p + (q - p) * 6 * t;
+            if(t < 1/2) return q;
+            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+        };
+
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+
+        r = hue2rgb(p, q, h + 1/3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1/3);
+    }
+
+    r = Math.round(r * 255);
+    g = Math.round(g * 255); 
+    b = Math.round(b * 255);
+
+    // Convert r b and g values to hex
+    rgb = b | (g << 8) | (r << 16); 
+    return "#" + (0x1000000 | rgb).toString(16).substring(1);
+}
+
 if (!localStorage.myChars){
     temp=[]
     localStorage.myChars= JSON.stringify(temp);
@@ -84,9 +159,11 @@ baseChars=[
     }
     roster = document.getElementById("canvas").getContext('2d');
     chars= baseChars.map(char=>makeChar.apply(this,char))        
+    onlyMine=false
+    compColor=false
     myChars=[]
     totChars=[]
-    xChars=9;
+    xChars=10;
     j=0;
     newName=""
     newURL=""
@@ -111,7 +188,7 @@ baseChars=[
     setColorTwo("#4B88E9")
     setMidPoint(30)
     resetNew();
-    setXChars(9);
+    setXChars(10);
     setResolution(window.innerWidth-24);
 
     function makeChar(name,order){
@@ -140,6 +217,7 @@ baseChars=[
                 img= new Image
                 img.src=char.image
                 arr[i].setImage=img
+    
             }
         })
     }
@@ -182,7 +260,8 @@ baseChars=[
             freeMove:true,
             onImageLoaded:(()=>setNewImage()),
             smallImage:"allow",
-            imageBackground:true, 
+            imageBackground:true,
+            originalSize:true,
             imageBackgroundBorderWidth: 15})
             drawTest();
         })
@@ -325,7 +404,11 @@ baseChars=[
         setDims()
         roster.font= defFont(0)
         loadMyChars()
-        totChars=chars.concat(myChars).filter(char=>char.render).sort( (a,b)=> a.order-b.order)
+        if (onlyMine){
+            totChars=myChars.filter(char=>char.render).sort( (a,b)=> a.order-b.order)    
+        }else{
+            totChars=chars.concat(myChars).filter(char=>char.render).sort( (a,b)=> a.order-b.order)
+        }
         charCount = echollapse?totChars.filter(c=>!c.echo).length:totChars.length
         function setDraw(char){
             if (char.setImage){
@@ -358,7 +441,15 @@ baseChars=[
             startX += ( defWidth * ( xChars - (charCount%xChars) ) ) / 2
         }
         startY=Math.floor(i/xChars)*defHeight
-        roster.fillStyle=grd
+        if(compColor){
+            color=colorThief.getPalette(char.setImage,8)[4]
+            console.log(color)
+            prim='#'+color[0].toString(16)+color[1].toString(16)+color[2].toString(16)
+            
+            roster.fillStyle=prim
+        }else{
+            roster.fillStyle=grd
+        }
         roster.fillRect(startX,startY,defWidth,defHeight)
         if (char.image) {
             roster.drawImage(char.setImage,startX,startY,defWidth,defHeight);
@@ -406,8 +497,9 @@ baseChars=[
             }
         }
         roster.fillStyle="white"
+        roster.font= defFont(6) 
         orderWidth=roster.measureText(char.order).width
-        if (showOrder) roster.fillText(char.order,startX+defWidth-orderWidth-2,startY+defWidth/12)
+        if (showOrder) roster.fillText(char.order,startX+defWidth-orderWidth-2,startY+defWidth/8)
         roster.lineWidth=1;
         roster.strokeStyle="black"
         if(char.echo && showEchoes){
@@ -415,7 +507,9 @@ baseChars=[
             roster.strokeText("\u03B5",startX+3,startY+defWidth/12+5)
             roster.font= defFont(0)
         }
-        if (showOrder) roster.strokeText(char.order,startX+defWidth-orderWidth-2,startY+defWidth/12)
+        roster.font= defFont(6)
+        if (showOrder) roster.strokeText(char.order,startX+defWidth-orderWidth-2,startY+defWidth/8)
+        roster.font= defFont(0)
         char.name!="RANDOM" && roster.strokeText(char.name.toUpperCase(),startX+textAdjust,startY+(defHeight*0.9),defWidth)
     }
 
@@ -496,6 +590,15 @@ baseChars=[
     }
     function toggleOrder(){
         showOrder=!showOrder
+        render()
+    }
+    function toggleColor(){
+        compColor=!compColor
+        render()
+    }
+    
+    function toggleMine(){
+        onlyMine=!onlyMine
         render()
     }
     function toggleEchoes(){
